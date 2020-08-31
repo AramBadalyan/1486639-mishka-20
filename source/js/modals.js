2712421b35d241cf396aa565542cb6bbcb2fb332
AramBadalyan <Arampitek@gmail.com>
2020-08-31 14:13:21 +0300

    Finished modal windows js logic

--- source/js/modals.js
+++ source/js/modals.js
@@ -1,12 +1,14 @@
 const modal_window = document.querySelector(".page-modal");
 const order_button = document.querySelector(".product__order-button");
 const add_button = document.querySelector(".to-cart__add-button");
-const toCart_buttons = document.querySelectorAll("goods-card__img");
+const toCart_buttons = document.querySelectorAll(".goods-card__to-cart");
 
-order_button.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal_window.classList.remove("hidden");
-});
+if (order_button) {
+  order_button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal_window.classList.remove("hidden");
+  });
+}
 
 for (let toCart_button of toCart_buttons) {
   toCart_button.addEventListener("click", function (evt) {
