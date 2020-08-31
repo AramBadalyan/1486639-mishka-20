612733853b70f03c2d01e7cdf72046b3d4c17dd1
AramBadalyan <Arampitek@gmail.com>
2020-08-31 14:05:47 +0300

    Added modal windows

---
+++ source/js/modals.js
@@ -0,0 +1,21 @@
+const modal_window = document.querySelector(".page-modal");
+const order_button = document.querySelector(".product__order-button");
+const add_button = document.querySelector(".to-cart__add-button");
+const toCart_buttons = document.querySelectorAll("goods-card__img");
+
+order_button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal_window.classList.remove("hidden");
+});
+
+for (let toCart_button of toCart_buttons) {
+  toCart_button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal_window.classList.remove("hidden");
+  });
+};
+
+add_button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal_window.classList.add("hidden");
+});
